a44622b2d61c9a822988c0bc2391024c32c9bc10
Gabriel Ting <gabrielfting@gmail.com>
2020-08-08 17:17:12 -0500

    Add common schema

---
+++ common/schema.js
@@ -0,0 +1,16 @@
+/**
+ * Exports shared schemas between client and server. Holds information specific to certain
+ * universities.
+ */
+
+export const demographicQuestions = [
+
+];
+
+export const commonQuestions = [
+  {
+    id: '',
+    question: '',
+    component: '',
+  },
+];
