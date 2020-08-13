29d8c12725324e8e47fd598cb76ea058b1c91cb4
Gabriel Ting <gabrielfting@gmail.com>
2020-08-13 14:29:59 -0500

    Strip styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
+import Theme from "./components/Theme"
 import { Survey } from './components/Survey';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
+            <Theme>
                 <Survey />
-            </header>
+            </Theme>
         </div>
     );
 }
--- client/src/components/Survey.jsx
+++ client/src/components/Survey.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Box, Flex, Button } from 'rebass'
+import {
+    Label,
+    Input,
+    Select,
+    Textarea,
+    Radio,
+    Checkbox,
+} from '@rebass/forms'
 
 /**
  * Frame that sets the logo
@@ -14,7 +23,7 @@ import { useForm } from 'react-hook-form';
  */
 
 // The following component is an example of your existing Input Component
-const Input = ({ label, register, required }) => (
+const CustomInput = ({ label, register, required }) => (
     <>
         <label>{label}</label>
         <input name={label} ref={register({ required })} />
@@ -22,7 +31,7 @@ const Input = ({ label, register, required }) => (
 );
 
 // you can use React.forwardRef to pass the ref too
-const Select = React.forwardRef(({ label, register }, ref) => (
+const CustomSelect = React.forwardRef(({ label, register }, ref) => (
     <>
         <label>{label}</label>
         <select name={label} ref={ref}>
@@ -37,10 +46,71 @@ export function Survey() {
     const onSubmit = (data) => console.log(data);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Input label="First Name" register={register} required />
-            <Select label="Age" ref={register} />
-            <input type="submit" />
-        </form>
+        <>
+            <Box
+                as='form'
+                onSubmit={e => e.preventDefault()}
+                py={3}>
+                <Flex mx={-2} mb={3}>
+                    <Box width={1/2} px={2}>
+                        <Label htmlFor='name'>Name</Label>
+                        <Input
+                            id='name'
+                            name='name'
+                            defaultValue='Jane Doe'
+                        />
+                    </Box>
+                    <Box width={1/2} px={2}>
+                        <Label htmlFor='location'>Location</Label>
+                        <Select
+                            id='location'
+                            name='location'
+                            defaultValue='NYC'>
+                            <option>NYC</option>
+                            <option>DC</option>
+                            <option>ATX</option>
+                            <option>SF</option>
+                            <option>LA</option>
+                        </Select>
+                    </Box>
+                </Flex>
+                <Flex mx={-2} flexWrap='wrap'>
+                    <Label width={[ 1/2, 1/4 ]} p={2}>
+                        <Radio
+                            id='beep'
+                            name='beep'
+                            value='beep'
+                            defaultChecked
+                        />
+                        Beep
+                    </Label>
+                    <Label width={[ 1/2, 1/4 ]} p={2}>
+                        <Radio
+                            id='boop'
+                            name='beep'
+                            value='boop'
+                        />
+                        Boop
+                    </Label>
+                    <Label width={[1/2, 1/4]} p={2}>
+                        <Checkbox
+                            id='remember'
+                            name='remember'
+                        />
+                        Remember Me
+                    </Label>
+                    <Box px={2} ml='auto'>
+                        <Button>
+                        Beep
+                        </Button>
+                    </Box>
+                </Flex>
+            </Box>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input label="First Name" register={register} required />
+                <Select label="Age" ref={register} />
+                <input type="submit" />
+            </form>
+        </>
     );
 }
