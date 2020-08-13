6025c6d59f896da435cabfa21584ce671a682687
Gabriel Ting <gabrielfting@gmail.com>
2020-08-13 14:37:14 -0500

    Strip out rebass again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
-import Theme from "./components/Theme"
 import { Survey } from './components/Survey';
 
 function App() {
     return (
         <div className="App">
-            <Theme>
-                <Survey />
-            </Theme>
+
+            <Survey />
+
         </div>
     );
 }
--- client/src/components/Survey.jsx
+++ client/src/components/Survey.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { Box, Flex, Button } from 'rebass'
-import {
-    Label,
-    Input,
-    Select,
-    Textarea,
-    Radio,
-    Checkbox,
-} from '@rebass/forms'
+
 
 /**
  * Frame that sets the logo
@@ -47,68 +39,10 @@ export function Survey() {
 
     return (
         <>
-            <Box
-                as='form'
-                onSubmit={e => e.preventDefault()}
-                py={3}>
-                <Flex mx={-2} mb={3}>
-                    <Box width={1/2} px={2}>
-                        <Label htmlFor='name'>Name</Label>
-                        <Input
-                            id='name'
-                            name='name'
-                            defaultValue='Jane Doe'
-                        />
-                    </Box>
-                    <Box width={1/2} px={2}>
-                        <Label htmlFor='location'>Location</Label>
-                        <Select
-                            id='location'
-                            name='location'
-                            defaultValue='NYC'>
-                            <option>NYC</option>
-                            <option>DC</option>
-                            <option>ATX</option>
-                            <option>SF</option>
-                            <option>LA</option>
-                        </Select>
-                    </Box>
-                </Flex>
-                <Flex mx={-2} flexWrap='wrap'>
-                    <Label width={[ 1/2, 1/4 ]} p={2}>
-                        <Radio
-                            id='beep'
-                            name='beep'
-                            value='beep'
-                            defaultChecked
-                        />
-                        Beep
-                    </Label>
-                    <Label width={[ 1/2, 1/4 ]} p={2}>
-                        <Radio
-                            id='boop'
-                            name='beep'
-                            value='boop'
-                        />
-                        Boop
-                    </Label>
-                    <Label width={[1/2, 1/4]} p={2}>
-                        <Checkbox
-                            id='remember'
-                            name='remember'
-                        />
-                        Remember Me
-                    </Label>
-                    <Box px={2} ml='auto'>
-                        <Button>
-                        Beep
-                        </Button>
-                    </Box>
-                </Flex>
-            </Box>
+            
             <form onSubmit={handleSubmit(onSubmit)}>
-                <Input label="First Name" register={register} required />
-                <Select label="Age" ref={register} />
+                <CustomInput label="First Name" register={register} required />
+                <CustomSelect label="Age" ref={register} />
                 <input type="submit" />
             </form>
         </>
