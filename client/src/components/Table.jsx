d207230b44bacfb5a4e75cc2b9486963d6e9047c
Gabriel Ting <gabrielfting@gmail.com>
2020-09-18 23:32:11 -0500

    Clean comments after ripping out Grid

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -25,28 +25,3 @@ export const Table = () => {
         </div>
     )
 }
-
-// import { Grid } from "gridjs";
-// import "gridjs/dist/theme/mermaid.css";
-// export const Table = () => {
-//     const wrapperRef = useRef(null);
-//     const state = useSelector(state => state)
-
-//     const grid = new Grid({
-//         columns: ['Unsent', 'Sent', 'In-progress', 'Completed', 'Total'],
-//         fixedHeader: true,
-//         authWidth: false,
-//         data: [
-//             [0,0,0,0,0]
-//             // [state.emails.unsent, state.emails.sent, state.emails.inprogress, state.emails.completed, state.emails.total]
-//         ]
-//     });
-  
-//     useEffect(() => {
-//         grid.render(wrapperRef.current);
-//     });
-  
-//     return (
-//         <div className="table" ref={wrapperRef}/>
-//     );
-// }
