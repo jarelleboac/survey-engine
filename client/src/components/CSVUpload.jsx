471e2c0afd1cee4a4556224caea3d83393b772a1
Gabriel Ting <gabrielfting@gmail.com>
2020-09-19 00:10:24 -0500

    Parse from text box

--- client/src/components/CSVUpload.jsx
+++ client/src/components/CSVUpload.jsx
@@ -1,16 +1,13 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef } from 'react'
 import {useSelector} from 'react-redux'
+import { readString } from 'react-papaparse'
 
 import CSVReader from 'react-csv-reader'
-import { Button } from 'theme-ui'
+import { Button, Label, Textarea, Box } from 'theme-ui'
 import {triggerToast, filterEmails} from '../utils'
-import {setCounts} from '../actions/email'
-
-// ${process.env.REACT_APP_API_URL}/email/${state.session.school}
 
 export const CSVUpload = ({setFreshData}) => {
-    const [emails, setEmails] = useState([])
-    const [uploaded, setUploaded] = useState(false)
+    const emailsRef = useRef()
     const [message, setMessage] = useState("")
     const state = useSelector(state => state)
 
@@ -20,11 +17,12 @@ export const CSVUpload = ({setFreshData}) => {
 
     const sendEmails = () => {
         
-        if (!uploaded) {
-            setMessage("Please upload a CSV containing emails first.");
+        if (emailsRef.current.length === 0) {
+            setMessage("Please upload a CSV containing emails first, or enter emails below.");
+            setMessage("")
             return;
         } 
-        const [validEmails, invalidEmails] = filterEmails(emails)
+        const [validEmails, invalidEmails] = filterEmails(emailsRef.current)
         if (invalidEmails.length !== 0) {
             setMessage("Please make sure your emails are valid emails.")
             // TODO: We should populate invalid emails section as a separate page
@@ -55,15 +53,39 @@ export const CSVUpload = ({setFreshData}) => {
             });
     }
 
+    const handleTextSubmit = (e) => {
+        const results = readString(e.target[0].value).data.flat()
+        emailsRef.current = results
+        sendEmails()
+    }
+
     return(
-        <div className="csv-upload">
-            <CSVReader onFileLoaded={(data) => {
-                setUploaded(true)
-                setEmails(data.flat())
-            }
-            } />
-            <Button onClick={sendEmails}>Upload</Button>
-        </div>
-    )
+        <>
+            <div className="csv-upload">
+                <CSVReader onFileLoaded={(data) => {
+                    emailsRef.current = data.flat()
+                }
+                } />
+                <Button onClick={sendEmails}>Upload</Button>
+            </div>
+            <Box
+                as='form'
+                onSubmit={e => {
+                    e.preventDefault() 
+                    handleTextSubmit(e)}
+                }>
+  
+                <Label htmlFor='text-emails'>Emails</Label>
+                <Textarea
+                    name='text-emails'
+                    id='text-emails'
+                    rows='6'
+                    mb={3}
+                />
+                <Button>
+    Submit
+                </Button>
+            </Box>
+        </>)
 
 }
