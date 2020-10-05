33c9bb3826fcef0ee4493ce62e3727cfaf710211
Gabriel Ting <gabrielfting@gmail.com>
2020-10-04 21:10:18 -0500

    Adjust naming of %admin page

---
+++ client/src/scenes/PercentAdmin/PercentAdminPanel.jsx
@@ -0,0 +1,12 @@
+import { Table } from '../../components/Table'
+import { CreateUser } from '../../components/CreateUser'
+import React from 'react'
+
+export const PercentAdminPanel = () => {
+    return (
+        <div className="admin-container">
+            <Table />
+            <CreateUser />
+        </div>
+    )
+}
