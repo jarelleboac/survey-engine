27a0d4e39734054eb20f706d2ce06a0b6874c133
Gabriel Ting <gabrielfting@gmail.com>
2020-10-11 09:54:09 -0500

    Add basic unsubscribe component

---
+++ client/src/scenes/Unsubscribe.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Heading,Button } from 'theme-ui'
+import { unsubscribe, triggerToast } from '../utils'
+
+
+export const Unsubscribe = ({token}) => {
+
+    const unsubscribeHandler = () => {
+        unsubscribe(token)
+            .then(res => res.json())
+            .then(res => {
+                if (res.error) {
+                    triggerToast(res.error)
+                } else if (res.message) {
+                    triggerToast(res.message)
+                }
+            })
+            .catch(err => {
+                triggerToast(err)
+            })
+    }
+
+    return (
+        <> 
+            <div className="container">
+                <div id="logo-container">
+                    <img src="logo.png" id="logo" alt="% project logo"/>
+                </div>
+
+                <header>
+                    <Heading>Unsubscribe</Heading>
+                </header>
+                <div className="default">
+                    <Button onClick={() => unsubscribeHandler()}>Unsubscribe</Button>
+                </div>
+            </div>
+        </>
+    );
+};
