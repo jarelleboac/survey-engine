d8c73655da657a0963044d741133247ac68a3f97
Megan Paik <paik.megan@gmail.com>
2020-11-02 20:34:34 -0600

    Revert "Styling changes"
    This reverts commit 220964c5fd81913ebf75066ffbf41db140bb4e61.

--- client/src/Theme.jsx
+++ client/src/Theme.jsx
@@ -2,51 +2,6 @@ import React from 'react'
 import { ThemeProvider } from 'theme-ui'
 import { funk } from '@theme-ui/presets'
 // import theme from '@theme-ui/preset-base'
-import DDinWoff from './assets/fonts/D-DIN.woff';
-import DDinBoldWoff from './assets/fonts/D-DIN-Bold.woff';
-
-const dDin = {
-    fontFamily: 'D-DIN',
-    fontStyle: 'normal',
-    fontDisplay: 'swap',
-    fontWeight: 400,
-    src: `
-    local('D-DIN'),
-    local('D-DIN-Regular'),
-    url(${DDinWoff}) format('woff')
-  `,
-};
-
-const dDinBold = {
-    fontFamily: 'D-DIN',
-    fontStyle: 'normal',
-    fontDisplay: 'swap',
-    fontWeight: 700,
-    src: `
-    local('D-DIN'),
-    local('D-DIN-Bold'),
-    url(${DDinBoldWoff}) format('woff')
-  `,
-};
-
-const fonts = [
-    'D-DIN',
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'Roboto',
-    'Oxygen',
-    'Ubuntu',
-    'Cantarell',
-    'Fire Sans',
-    'Droid Sans',
-    'Helvetica Neue',
-    'Arial',
-    'sans-serif',
-    'Apple Color Emoji',
-    'Segoe UI Emoji',
-    'Segoe UI Symbol',
-].join(',')
 
 // example theme.js
 const funkTheme = {
@@ -54,32 +9,6 @@ const funkTheme = {
     styles: {
         ...funk.styles,
     },
-    typography: {
-        fontFamily: fonts,
-    },
-    fonts: {
-        body: 'D-DIN',
-        heading: 'D-DIN'
-    },
-    overrides: {
-        MuiCssBaseline: {
-            '@global': {
-                '@font-face': [dDin,dDinBold],
-            },
-        },
-    },
-    forms: {
-        input: {
-            border: 'none',
-            borderBottom: '1px solid rgba(0, 0, 0, .4)',
-            borderRadius: '0px',
-            marginLeft: '1em',
-            outline: 'none',
-            maxWidth: '40%',
-            padding: '0 0 0 0',
-            fontFamily: fonts
-        }
-    }
 }
 
 // const themeObj = {
