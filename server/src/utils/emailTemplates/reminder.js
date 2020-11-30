824f086e7aab76e311c013d569dadae5895fed71
Megan Paik <paik.megan@gmail.com>
2020-11-30 09:30:14 -0600

    Specify return path for emails

--- server/src/utils/emailTemplates/reminder.js
+++ server/src/utils/emailTemplates/reminder.js
@@ -59,4 +59,5 @@ export default (user, surveyUrl, school, senderEmail, unsubscribeUrl) => ({
     ReplyToAddresses: [
         `The Percentage Project <${senderEmail}>`,
     ],
+    ReturnPath: 'program@percentageproject.com',
 });
